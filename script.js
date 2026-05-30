cf356aa86063de7e472994c60a7093e093cf5782
NayanPython <choudharyji527@gmail.com>
2026-05-30 09:32:47 +0530

    dummy title in script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+/*   1. THE DATA LAYER */
+const projectsData = [
+    {
+        title: "Algorithmic Trading Engine",
+        description: "High-frequency execution bot utilizing Fyers API for millisecond order routing and market data analysis.",
+        techStack: ["Python", "WebSockets", "Pandas"],
+        githubLink: "#"
+    },
+    {
+        title: "Campus Placement API",
+        description: "Decoupled backend architecture handling role-based access, concurrent user applications, and relational data.",
+        techStack: ["Flask", "PostgreSQL", "REST API"],
+        githubLink: "https://github.com/Nayanxyz/Nayanxyz-Portfolio.git"
+    },
+    {
+        title: "Headless Extraction Bot",
+        description: "Automated data pipeline utilizing Selenium to bypass basic bot-detection and scrape dynamic DOM elements.",
+        techStack: ["Python", "Selenium", "Data Automation"],
+        githubLink: "#"
+    }
+];
+
